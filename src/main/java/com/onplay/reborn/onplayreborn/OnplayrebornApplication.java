e13d927805dcccef84433a6fd08cb60944ea1108
cauagran <cauagran@gmail.com>
2025-10-08 17:55:53 -0300

    first commit

---
+++ src/main/java/com/onplay/reborn/onplayreborn/OnplayrebornApplication.java
@@ -0,0 +1,13 @@
+package com.onplay.reborn.onplayreborn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnplayrebornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnplayrebornApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onplay/reborn/onplayreborn/OnplayrebornApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onplay.reborn.onplayreborn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnplayrebornApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
